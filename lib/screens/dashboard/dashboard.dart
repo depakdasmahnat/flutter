b6cdb5a6ea5e4d782622aa36d57f4c08c0e64b9f
deepakdas mahant <113573176+depakdasmahnat@users.noreply.github.com>
2024-02-09 18:31:28 +0530

    check lead

--- lib/screens/dashboard/dashboard.dart
+++ lib/screens/dashboard/dashboard.dart
@@ -199,17 +199,13 @@ class DashBoardState extends State<DashBoard> {
 
 class CustomBottomNavBar extends StatelessWidget {
   final int index;
-
   final int dashBoardIndex;
-
   final double? height;
   final double? width;
   final bool? alwaysShowLabel;
-
   final DashboardData data;
   final GestureTapCallback? onTap;
   final EdgeInsets? imageMargin;
-
   const CustomBottomNavBar({
     super.key,
     required this.index,
@@ -225,7 +221,6 @@ class CustomBottomNavBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     bool selected = dashBoardIndex == index;
-
     return GestureDetector(
       onTap: onTap ??
           () {
