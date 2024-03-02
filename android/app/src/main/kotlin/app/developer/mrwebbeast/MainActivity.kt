20ce23a0df595945042c8009c2d0e83a990a9a2a
Sahil Ranout <mrwebbeast@gmail.com>
2024-03-02 18:54:29 +0530

    updates

--- android/app/src/main/kotlin/app/developer/mrwebbeast/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package app.gtp.com
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity() {
-}
--- lib/core/extensions/nullsafe/null_safe_list_extentions.dart
+++
@@ -1,6 +0,0 @@
-extension NullSafeListExtenstion on List? {
-  ///To Find that a Null List have data...
-  bool get haveData {
-    return ((this?.length ?? 0) > 0);
-  }
-}
